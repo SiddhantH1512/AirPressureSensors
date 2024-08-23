aeb92720d3167677828550890b4feb842a96f20a
Siddhant <siddhanthardikar6@gmail.com>
2024-08-23 11:25:23 -0500

    logger and exception handling file created

---
+++ sensor/exception.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+def error_message_detail(error, error_detail: sys):
+    _,_,exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+    error_message = f"Error occure in script name {filename}, Line number {line_no} and error message is str(error)"
+
+    return error_message
+
+
+class SensorException(Exception):
+    def __init__(self, error, error_message: sys):
+        super().__init__(error)
+        self.error = error
+        self.error_message = error_message_detail(error, error_message)
+
+    def __str__(self):
+        return self.error_message
---
+++ sensor/logger.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+import datetime
+
+
+LOG_FILE_NAME = f"{datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="w"
+)
+
+LOGGING = logging.getLogger()
